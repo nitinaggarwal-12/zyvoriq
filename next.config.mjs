baac5c1147c8f5d82bf8a971d05a0f1650ce92b3
Nitin Aggarwal <nitinaggarwal12@gmail.com>
2026-08-26 23:06:28 -0400

    feat(config): allow proxy.googlers.com dev origins and ensure clean cache

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  experimental: {
+    allowedDevOrigins: [
+      "localhost:3000",
+      "*.proxy.googlers.com",
+      "*.c.googlers.com"
+    ]
+  }
 };
 
 export default nextConfig;
