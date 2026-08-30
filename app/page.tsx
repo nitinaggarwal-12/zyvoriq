436cb4f2b5d80f465750b0c2714f6ce5a3bc40ee
Nitin Aggarwal <53987770+nitinaggarwal-12@users.noreply.github.com>
2026-08-29 21:40:42 -0400

    Redesign public homepage around creation outcomes

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,17 @@
 import React from "react";
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
-import { DirectorConsole } from "@/components/DirectorConsole";
-import { LifecycleFlow } from "@/components/LifecycleFlow";
-import { VeritasQualityMatrix } from "@/components/VeritasQualityMatrix";
 import { MultimodalStudio } from "@/components/MultimodalStudio";
-import { AutonomyPolicyControls } from "@/components/AutonomyPolicyControls";
 import { UseCasesSection } from "@/components/UseCasesSection";
 import { WaitlistCTA } from "@/components/WaitlistCTA";
 import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-obsidian-950 text-slate-100 selection:bg-teal-500/30 selection:text-teal-200">
+    <main id="top" className="min-h-screen bg-obsidian-950 text-slate-100 selection:bg-teal-500/30 selection:text-teal-100">
       <Navbar />
       <Hero />
-      <DirectorConsole />
-      <LifecycleFlow />
-      <VeritasQualityMatrix />
       <MultimodalStudio />
-      <AutonomyPolicyControls />
       <UseCasesSection />
       <WaitlistCTA />
       <Footer />
