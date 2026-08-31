595b4db63983c3265180d15a892cd0f7d094e96c
Nitin Aggarwal <53987770+nitinaggarwal-12@users.noreply.github.com>
2026-08-31 11:03:47 -0400

    fix(studio1): expose full reel resync recovery

--- app/studio1/page.tsx
+++ app/studio1/page.tsx
@@ -1,5 +1,6 @@
 import { Studio1 } from "./Studio1";
+import { Studio1ResyncControl } from "./Studio1ResyncControl";
 
 export default function Studio1Page() {
-  return <Studio1 />;
+  return <><Studio1 /><Studio1ResyncControl /></>;
 }
